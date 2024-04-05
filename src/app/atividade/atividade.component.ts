d5a083f704fb372f942773360271253c5274374d
renatachom <renatachom.ads@gmail.com>
2024-04-05 13:52:50 -0300

    Testando

---
+++ src/app/atividade/atividade.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atividade',
+  standalone: true,
+  imports: [],
+  templateUrl: './atividade.component.html',
+  styleUrl: './atividade.component.css'
+})
+export class AtividadeComponent {
+
+}
